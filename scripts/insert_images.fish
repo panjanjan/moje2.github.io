a6fce79f9acb9551fb3c20cfdde8b1abd7aa8ab7
panjanjan <jan.panjan@gmail.com>
2026-08-15 15:29:11 +0200

    auto

--- scripts/insert_images.fish
+++ scripts/insert_images.fish
@@ -1,13 +1,26 @@
 #!/bin/fish
-if ! set -q $argv[2]
-    echo "./insert_images.fish <dir>"
+argparse d/dir f/file -- $argv || exit
+
+if ! set -ql _flag_dir
+or ! set -ql _flag_file
+or test \( "$argv[1]" = "" -o "$argv[2]" = "" \)
     echo ""
-    echo "dir je absolute path do slik relativno glede na root"
-    echo "slike morajo biti jpg"
+    echo "./insert_images.fish <dir> <file>"
+    echo ""
+    echo "vstavi slike v HTML dokument. slike morajo biti jpg."
+    echo "pazi da ne poženeš večkrat."
+    echo ""
+    echo "dir:  absolute path do slik"
+    echo "file: absolute path do HTML dokumenta"
     exit
 end
 
-for f in (find assets/images/ch1/ -type f | sort)
+set dir $argv[1]
+set file $argv[2]
+
+for f in (find $dir/ -type f | sort)
       set n (path basename $f | string replace ".jpg" "")
-      echo "<img src=\"$f\" alt=\"ch 1 p $n\">" >> ch1.html
+      echo "<img src=\"$f\" alt=\"ch 1 p $n\">" >> "$file"
 end
+
+cat $file
